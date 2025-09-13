af49730b21f6637e117639ca83f75d3324cd8aba
William Doran <wfdoran4@yahoo.com>
2025-09-13 15:05:51 -0700

    More from chapter 3

--- Chapter3/src/main.rs
+++ Chapter3/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     sub3_1_1();
     sub3_1_2();
+    sub3_1_3();
+    sub3_2();
 }
 
 fn sub3_1_1() {
@@ -52,3 +54,52 @@ fn sub3_1_2() {
    
 }
 
+fn sub3_1_3() {
+    let random_tuple = ("string", 8, 4.3, vec!['a'], [7,8,9]);
+    println!("{:?}", random_tuple);
+    println!("{}", random_tuple.1);
+
+    let (_, _, a, _, b) = random_tuple;
+    println!("{}", a);
+    println!("{:?}", b);
+
+}
+
+fn sub3_2() {
+    let x = 5;
+    if x == 7 {
+        println!("yes");
+    } else {
+        println!("no");
+    }
+
+    let y: u8 = 5;
+    match y {
+        0 | 1 | 2 => println!("really small"),
+        4 | 5 | 6 => println!("middle"),
+        _ => println!("big!"),
+    }
+
+    let z = match y {
+        0 => 0,
+        1 => 1,
+        2 => 4,
+        3 => 9,
+        4 => 16,
+        5 => 25,
+        _ => -1,
+    };
+    println!("{z}");
+
+    match (x, z) {
+        (x,z) if x > 0 && z >= 0 => println!("both positive"),
+        (x,_) if x >= 0 => println!("only x"),
+        (_,z) if z >= 0 => println!("only z"),
+        _ => println!("neither"),
+    };
+
+    match y {
+        y @ 0..=127 => println!("{y} is low"),
+        y @ 128..=255 => println!("{y} is hi"),
+    }
+}
