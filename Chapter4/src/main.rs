f6d6580a67f5db88c504322fda0312377faf204f
William Doran <wfdoran4@yahoo.com>
2025-09-20 14:58:50 -0700

    Finish section 4.1

--- Chapter4/src/main.rs
+++ Chapter4/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
     sub4_1();
+    sub4_1_3();
+    sub4_1_5();
 }
 
 #[derive(Debug)]
@@ -47,6 +49,14 @@ fn time_to_thing(time_hour: i32) -> Things2 {
     }
 }
 
+fn time_to_thing2(time_hour: i32) -> Things2 {
+    use Things2::*;
+    match time_hour {
+        6..=18 => Sun(String::from("sun")),
+        _ => Things2::Stars(7),
+    }
+}
+
 fn sub4_1() {
     let red = ColorRGB(255, 0, 0);
     let x = SizeAndColor {
@@ -73,8 +83,69 @@ fn sub4_1() {
 
     println!("{:?} {:?}", time_to_sky(12), time_to_sky(20));
 
+    use Things2::*;
     match time_to_thing(12) {
-        Things2::Sun(s) => println!("{s}"),
-        Things2::Stars(n) => println!("stars: {n}"),
+        Sun(s) => println!("{s}"),
+        Stars(n) => println!("stars: {n}"),
     }
+
+    match time_to_thing2(3) {
+        Sun(s) => println!("{s}"),
+        Stars(n) => println!("stars: {n}"),
+    }
+}
+
+enum Season {
+    Spring,
+    Summer,
+    Autumn,
+    Winter,
+}
+
+fn sub4_1_3() {
+    use Season::*;
+    let four_seasons = vec![Spring, Summer, Autumn, Winter];
+    for season in four_seasons {
+        println!("{}", season as u32);
+    }
+}
+
+
+#[derive(Debug)]
+enum AnimalType {
+    Cat,
+    Dog,
+}
+
+#[derive(Debug)]
+struct Animal {
+    age: u8,
+    atype: AnimalType,
+}
+
+impl Animal {
+    fn new_cat() -> Self {
+        Self {
+            age: 0,
+            atype: AnimalType::Cat,
+        }
+    }
+
+    fn to_dog(&mut self) {
+        self.atype = AnimalType::Dog;
+    }
+    fn to_cat(&mut self) {
+        self.atype = AnimalType::Cat;
+    }
+    fn age(&mut self, extra: u8) {
+        self.age += extra;
+    }
+}
+
+fn sub4_1_5() {
+    let mut x = Animal::new_cat();
+    println!("{:?}", x);
+    x.to_dog();
+    x.age(10);
+    println!("{:?}", x);
 }
