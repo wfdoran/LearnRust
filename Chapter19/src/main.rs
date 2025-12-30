4c5f01f4e60d615f835f3609c091985d408dc4ec
William Doran <wfdoran4@yahoo.com>
2025-12-29 19:34:52 -0800

    Chapter 19.  19.3.4 more details

--- Chapter19/src/main.rs
+++ Chapter19/src/main.rs
@@ -1,4 +1,7 @@
 use tokio;
+use rand::*;
+use tokio::join;
+use std::time::Duration;
 
 async fn get_val() -> u8 {
     8
@@ -8,4 +11,25 @@ async fn get_val() -> u8 {
 async fn main() {
     let x = get_val().await;
     println!("{}", x);
+
+    get_many().await;
+
+}
+
+async fn get_val2(num: u8) -> u8 {
+    let mut rng = rand::rng();
+    let wait_time = rng.random_range(1..100);
+    tokio::time::sleep(Duration::from_millis(wait_time)).await;
+    println!("got {}", num);
+    num
+}
+
+async fn get_many() {
+    let nums = join!(
+        get_val2(1), 
+        get_val2(2),
+        get_val2(3)
+    );
+
+    println!("{nums:?}");
 }
